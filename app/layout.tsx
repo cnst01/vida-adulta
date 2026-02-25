850c299339158100eed27b9c5072e931aa67ebff
cnst01 <cassionst@gmail.com>
2026-02-25 01:26:49 -0300

    translate blocker added

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Vida Adulta",
   description: "Controle financeiro inteligente",
+  other: {
+    google: "notranslate",
+  },
 };
 
 export default function RootLayout({
